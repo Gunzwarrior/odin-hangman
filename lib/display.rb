78579f8f6a0c9711f47fb23e9fbe71786931a09a
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-12-07 10:46:49 +0100

    Create solution picker method

--- lib/display.rb
+++ lib/display.rb
@@ -8,12 +8,17 @@ module Display
       You will have to guess the word by guessing
       a letter one at a time.
       You can have 7 wrong guesses before you lose.
-      Enter a letter if you want to start guessing,
-      exit if you want to save and exit, or load if
-      you want to load your previous saved game.
-
     RULES
 
+  end
+
+  def command_list
+    <<~COMMAND_LIST
+      - To start a new game, press Enter
+      - To load your previous saved game, type 'load'.
+      - To save and exit, type 'exit'.
+
+    COMMAND_LIST
   end
 
   def prompt
--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,45 @@
-
 require_relative 'display'
+require_relative 'player'
 
 # Game class containing the game's logic 
 class Game
   include Display
 
+  attr_accessor :solution
+
   def initialize
-    @solution = 0
+    @solution = pick_solution
+    @player = Player.new
   end
 
   def play
     puts rules
+    puts command_list
+  end 
+
+  def select_game(choice)
+    keep_going = true
+    while keep_going
+      if choice == ''
+        # start new game
+      elsif choice.downcase == 'exit'
+        # exit game
+      elsif choice.downcase == 'load'
+        # load game
+      else
+        puts command_list
+      end
+    end
+  end
+
+  def pick_solution
+    solution_string = open('google_10000_english_no_swears.txt', 'r')
+    solution_array = []
+    until solution_string.eof?
+      line = solution_string.readline.chomp
+      solution_array.push(line) if line.length >= 5 && line.length <= 12
+    end
+    solution_array.sample
   end
 
 end
